ddf7969524b4e277a5b19667d86f8890f1aeb53c
Nikolai "Scripto <34796034+scripto2012@users.noreply.github.com>
2021-01-22 03:00:16 +0500

    New file Add.html

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class Item(db.Model):
@@ -13,6 +14,7 @@ class Item(db.Model):
     status = db.Column(db.Boolean, default=True)
     # desc = db.Column(db.Text, nullable=True)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -21,6 +23,25 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        # desc = request.form['desc']
+
+        item = Item(name=name, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ошибка'
+    else:
+        return render_template('add.html')
 
 
 if __name__ == "__main__":
