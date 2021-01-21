d7ac59d9c78eb49dec10b10647b63ffa472240e9
Nikolai "Scripto <34796034+scripto2012@users.noreply.github.com>
2021-01-22 04:54:59 +0500

    version 1

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+from cloudipsp import Api, Checkout
+
 app = Flask(__name__)
-app.config['SQLALCHEMY DATABASE_URI'] = 'sqlite:///shop.db'
-app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Item(db.Model):
+class ItemTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Boolean, default=True)
-    # desc = db.Column(db.Text, nullable=True)
+    desc = db.Column(db.String(1000), nullable=False)
 
+    def __repr__(self):
+        return self.name
+
+
+@app.route('/buy/<int:id>')
+def buy(id):
+    item = ItemTable.query.get(id)
+
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "RUB",
+        "amount": str(item.price) + "00"
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = ItemTable.query.all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
@@ -30,9 +50,9 @@ def add():
     if request.method == 'POST':
         name = request.form['name']
         price = request.form['price']
-        # desc = request.form['desc']
+        desc = request.form['desc']
 
-        item = Item(name=name, price=price)
+        item = ItemTable(name=name, price=price, desc=desc)
 
         try:
             db.session.add(item)
